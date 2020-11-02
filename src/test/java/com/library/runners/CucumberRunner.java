e96c410bf3239a914bd357ef9bfaa4d808a8734b
romantrust <romaaakrytskyi@gmail.com>
2020-11-02 14:07:43 -0600

    userStory1AC1/2/3/4

--- src/test/java/com/library/runners/CucumberRunner.java
+++ src/test/java/com/library/runners/CucumberRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/library/step_definitions",
         dryRun = false,
-        tags = "@userStory1Ac1"
+        tags = "@libraryUserStory1"
 
 
 )
