b711d232b25eb11d57933666acba60141d970328
Volodymyr0077 <v.pisarzhevsky@gmail.com>
2020-11-02 14:15:36 -0600

    BooksPage US3, AC1,2

---
+++ src/test/java/com/library/pages/BooksPage.java
@@ -0,0 +1,81 @@
+package com.library.pages;
+
+import com.github.javafaker.Faker;
+import com.library.utils.Driver;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.library.utils.BrowserUtils.clickOnElement;
+import static com.library.utils.BrowserUtils.clickWithJS;
+
+public class BooksPage extends BasePage {
+    Faker faker=new Faker();
+    public BooksPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(linkText = "Add Book")
+    private WebElement addBookButton;
+
+
+    public void addBookButtonVerification() {
+        Assert.assertTrue(addBookButton.isDisplayed());
+    }
+
+    public void clickingAddBookButton() {
+        clickOnElement(addBookButton);
+    }
+
+    @FindBy(xpath = "//th[@data-name]")
+    private List<WebElement> headerElements;
+
+    public void headerElementsVerification(List<String> dataTable) {
+        List<String> headerElementsString = new ArrayList<>();
+
+        for (WebElement each : headerElements) {
+            headerElementsString.add(each.getText().trim());
+        }
+        Assert.assertEquals("Header verification failed", dataTable, headerElementsString);
+    }
+
+    @FindBy(xpath = "//input[@placeholder='Book Name']")
+    private WebElement bookNameField;
+
+    @FindBy(xpath = "//input[@placeholder='ISBN']")
+    private WebElement isbnField;
+
+    @FindBy(xpath = "//input[@placeholder='Year']")
+    private WebElement yearField;
+
+    @FindBy(xpath = "//input[@placeholder='Author']")
+    private WebElement authorField;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    private WebElement saveChanges;
+
+public void fillingUpRequiredFields(){
+
+    bookNameField.sendKeys(faker.book().title());
+
+    isbnField.sendKeys(faker.idNumber().invalid());
+
+    yearField.sendKeys(faker.date().birthday().toString());
+
+    authorField.sendKeys(faker.name().fullName());
+
+}
+public void clickingSaveChanger(){
+    clickOnElement(saveChanges);
+}
+
+
+
+
+
+}
