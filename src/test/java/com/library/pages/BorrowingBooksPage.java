cf2aa0f086e85e8cb63c8bff1c7d893428ae3beb
rumeysakdogan <dgn.rumeysaa@gmail.com>
2020-11-02 15:59:53 -0700

    added BorrowingBooksStepDefinitions

--- src/test/java/com/library/pages/BorrowingBooksPage.java
+++ src/test/java/com/library/pages/BorrowingBooksPage.java
@@ -1,4 +1,21 @@
 package com.library.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class BorrowingBooksPage {
+
+    @FindBy(xpath = "//a[@onclick='BorrowingBooks.return_book(718)']")
+    private WebElement returnBookButton;
+
+    @FindBy(id = "toast-container")
+    private WebElement successMessage;
+
+    public void clickOnReturnBookButton(){
+        returnBookButton.click();
+    }
+
+    public void successMessageIsDisplayed(){
+        successMessage.isDisplayed();
+    }
 }
---
+++ src/test/java/com/library/step_definitions/BorrowingBooksStepDefinitions.java
@@ -0,0 +1,23 @@
+package com.library.step_definitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class BorrowingBooksStepDefinitions {
+
+    @When("user navigate to Borrowing Books module")
+    public void user_navigate_to_borrowing_books_module() {
+
+    }
+
+    @When("user click on return book button")
+    public void user_click_on_return_book_button() {
+
+    }
+
+    @Then("user should see {string} message")
+    public void user_should_see_message(String string) {
+
+    }
+
+}
